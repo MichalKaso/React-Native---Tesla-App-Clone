15399f6471b6ceb44003fb22273272c36294e7ea
Mike <michalkaso@yahoo.com>
2022-10-12 10:28:33 +0100

    CarItem Component

---
+++ components/CarItem/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import {View, Text, ImageBackground} from 'react-native';
+import styles from "./styles";
+
+const CarItem = (props) => {
+    return(
+        <View style={styles.carContainer}>
+
+        <ImageBackground source={require('../../assets/images/ModelX.png')}
+
+        style={styles.image}
+        />
+
+        <View style={styles.titles}>
+          <Text style={styles.title}>Model S</Text>
+          <Text style={styles.subtitle}>Starting at €69,420 </Text>
+        </View>
+      </View>
+    );
+};
+
+export default CarItem;
