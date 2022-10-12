847ef6c326fc60391bf58a3fdb6299ba58f77291
Mike <michalkaso@yahoo.com>
2022-10-12 12:23:43 +0100

    Rendering Final App Content

---
+++ components/CarsList/cars.js
@@ -0,0 +1,28 @@
+export default [{
+    name: 'Model S',
+    tagline: 'Starting at €69,420',
+    image: require('../../assets/images/ModelSblue.png'),
+}, 
+
+{
+    name: 'Model 3',
+    tagline: 'Order Online For',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/Model3.png'),
+}, 
+
+{
+    name: 'Model X',
+    tagline: 'Order Online For',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/ModelX.png'),
+},
+
+{
+    name: 'Model Y',
+    tagline: 'Order Online For',
+    taglineCTA: 'Touchless Delivery',
+    image: require('../../assets/images/ModelY.png'),
+},
+
+];
